6270950d82b3f8d89e1f31504f16c7a1aa61d3b0
feng295 <d11405006@ems.npu.edu.tw>
2025-12-01 20:22:02 +0800

    v2.1.239 修改個人資訊

--- script.js
+++ script.js
@@ -1766,7 +1766,7 @@ document.addEventListener("DOMContentLoaded", async function () {
             try {
                 const token = getToken();
                 if (!token) throw new Error("認證令牌缺失，請重新登入！");
-                const response = await fetch(`${API_URL}/members/id`, {
+                const response = await fetch(`${API_URL}/members/${memberId}`, {
                     method: 'PUT',
                     headers: {
                         "Content-Type": "application/json",
